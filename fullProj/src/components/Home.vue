2dc4be52f548f57ef674a338fd76f7fc51d37a34
zanny88 <117763771+zanny88@users.noreply.github.com>
2024-08-26 17:56:38 +0200

    aggiunti filtri per nella pagina delle note

--- fullProj/src/components/Home.vue
+++ fullProj/src/components/Home.vue
@@ -1,20 +1,6 @@
 <template>
-    <!--
     <ul>
         <li>Markdown nelle note</li>
         <li>Filtri/ordine nelle note</li>
-        <li>Creare to-do dal corpo delle note</li>
     </ul>
-    -->
-    <p>Hello</p>
 </template>
-
-<!--
-Palette 1:
-#dda15e, #60d394, #aaf683, #330f0a, #394f49
-
--->
-
-<style scoped>
-
-</style>
