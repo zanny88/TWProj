929e4524dffeef0807a41c2da54b2d063320d0b1
zanny88 <117763771+zanny88@users.noreply.github.com>
2024-11-09 10:00:28 +0100

    setup server disi

--- fullProj/userModel.js
+++ fullProj/userModel.js
@@ -4,6 +4,7 @@ const {messageSchema} = require('./messageModel');
 
 const mongoDBUri = "mongodb+srv://marcostignani9:qpalzmQP8@clusternote.yd03buh.mongodb.net/?retryWrites=true&w=majority&appName=ClusterNote";
 //const mongoDBUri = "mongodb+srv://giarrussolorenzo:t1otEqlgBECuv4NL@cluster0.hqzaedi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//const mongoDBUri = "mongodb://site232415:eib8PaiP@mongo_site232415/?authSource=admin&writeConcern=majority";
 mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = new mongoose.Schema({
