d385a98a1250f59695ce6032143c63ccc80dea2e
zanny88 <117763771+zanny88@users.noreply.github.com>
2024-12-31 12:23:58 +0100

    modifica gestione messaggi

--- fullProj/userModel.js
+++ fullProj/userModel.js
@@ -1,15 +1,19 @@
+// userModel.js
 const mongoose = require("mongoose");
-const { messageSchema } = require('./messageModel');
-require('dotenv').config();
-const mongoDBUri = process.env.MONGODB_URI;
+const {messageSchema} = require('./messageModel');
 
+const mongoDBUri = "mongodb+srv://marcostignani9:qpalzmQP8@clusternote.yd03buh.mongodb.net/?retryWrites=true&w=majority&appName=ClusterNote";
+//const mongoDBUri = "mongodb+srv://giarrussolorenzo:t1otEqlgBECuv4NL@cluster0.hqzaedi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//const mongoDBUri = "mongodb://site232415:eib8PaiP@mongo_site232415/?authSource=admin&writeConcern=majority";
 mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    username: String,
     passw: String,
+    name: String,
+    mail: String,
     friends: { type: [String], default: [] },
-    inbox: { type: [messageSchema], default: [] }
+    inbox: {type: [messageSchema], default: []}
 });
 
 const User = mongoose.model("User", userSchema);
