468687627fec4f710a773f50b1e3ab430ed76e57
zanny88 <117763771+zanny88@users.noreply.github.com>
2025-01-05 12:04:47 +0100

    send current time to server

--- fullProj/src/stores/timeMachine.js
+++ fullProj/src/stores/timeMachine.js
@@ -1,16 +1,20 @@
 import { defineStore } from 'pinia';
 import dayjs from 'dayjs';
+import axios from 'axios';
+const api_url = 'http://localhost:3000/';
 
 export const useTimeMachineStore = defineStore('timeMachine', {
     state: () => ({
         currentTime: dayjs(), // Initialize with the current date and time
     }),
     actions: {
-        setTime(newTime) {
+        async setTime(newTime) {
             this.currentTime = dayjs(newTime);
+            await axios.get(`${api_url}setTime?time=${this.currentTime.toISOString()}`);
         },
-        resetTime() {
+        async resetTime() {
             this.currentTime = dayjs();
+            await axios.get(`${api_url}setTime?time=${this.currentTime.toISOString()}`);
         },
     },
     getters: {
