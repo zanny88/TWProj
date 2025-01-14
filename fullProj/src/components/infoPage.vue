96b4c321c7fe04c6020b4ebce4c1229e92eae744
zanny88 <117763771+zanny88@users.noreply.github.com>
2025-01-14 11:53:19 +0100

    modifiche view cambio dati utente

--- fullProj/src/components/infoPage.vue
+++ fullProj/src/components/infoPage.vue
@@ -1,5 +1,10 @@
 <template>
     <Modal v-if="changeData">
+        <div style="position: relative; display: flex; justify-content: flex-end;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x" id="del_friend" viewBox="0 0 16 16" @click="changeData = false">
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+            </svg>
+        </div>
         <div class="row" style="position: relative; justify-content: center; align-items: center;">
             <div class="col-lg-4 col-md-4 col-sm-8">
                 <div class="form-floating">
@@ -17,16 +22,20 @@
         <div class="row" style="position: relative; justify-content: center; align-items: center;">
             <div class="col-lg-8 col-md-8 col-sm-12">
                 <div class="form-floating">
-                    <input type="text" id="username" name="username" class="form-control" v-model="userInfo[2]" required/>
+                    <input type="text" id="username" name="username" class="form-control" v-model="userInfo[2]" @input="showDismissibleAlert = false" required/>
                     <label for="username">Username: </label>
                 </div>
             </div>
         </div>
+        <div v-show="showDismissibleAlert" style="background: red; color: black;">
+            Username già in uso!
+        </div>
         <div class="row" style="position: relative; justify-content: center; align-items: center;">
             <div class="col-lg-8 col-md-8 col-sm-12">
-                <div class="form-floating">
-                    <input type="password" id="passwd" name="passwd" class="form-control" v-model="userInfo[3]" required/>
+                <div class="form-floating" style="position: relative;">
+                    <input :type="passwordType" id="passwd" name="passwd" class="form-control" v-model="userInfo[3]" required/>
                     <label for="passwd">Password:</label>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" :class="['bi','eye-icon',eye_class]" viewBox="0 0 16 16" @click="passwVisib()" v-html="eye_icon"></svg>
                 </div>
             </div>
         </div>
@@ -64,9 +73,35 @@ const user = atob(localStorage.getItem('token').split('.')[1]);
 const router = useRouter();
 var userInfo = ref([]);
 var changeData = ref(false);
+var showDismissibleAlert = ref(false);
+
+var passwordType = ref('password');
+var eye_class = ref('bi-eye-slash-fill');
+var eye_icon = ref(`
+    <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
+    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
+`);
+
+function passwVisib(){
+    if(passwordType.value == 'password'){
+        passwordType.value = 'text';
+        eye_icon.value = `
+            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+        `;
+        eye_class.value = 'bi-eye-fill';
+    }else{
+        passwordType.value = 'password';
+        eye_icon.value = `
+            <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
+            <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
+        `;
+        eye_class.value = 'bi-eye-slash-fill';
+    }
+    
+}
 
 async function saveUserData(){
-    changeData.value = false;
     const newUserData = {
         name: userInfo.value[0].concat(" ",userInfo.value[1]),
         oldUsername: user,
@@ -82,7 +117,12 @@ async function saveUserData(){
             r = await axios.post(`${api_url}user/updateData`,newUserData);
             const newToken = r.data.token;
             localStorage.setItem('token',newToken);
+            showDismissibleAlert.value = false;
+            changeData.value = false;
             router.go(0);
+        }else{
+            userInfo.value[2] = "";
+            showDismissibleAlert.value = true;
         }
     }catch(error){
         console.log("Errore: ",error);
@@ -109,3 +149,16 @@ onMounted(() => {
     getUserInfo();
 })
 </script>
+
+<style scoped>
+    .bi-x:hover{
+        color: red;
+    }
+    .eye-icon{
+        position: absolute;
+        right: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+        cursor: pointer;
+    }
+</style>
--- fullProj/src/components/registerLogin.vue
+++ fullProj/src/components/registerLogin.vue
@@ -32,9 +32,10 @@
             </div>
             <div class="row" style="position: relative; justify-content: center; align-items: center;">
                 <div class="col-lg-4 col-md-4 col-sm-12">
-                    <div class="form-floating">
-                        <input type="password" v-model="passwd" id="passwd" name="passwd" class="form-control" @input="hideDiv()" required/>
+                    <div class="form-floating" style="position: relative;">
+                        <input :type="passwordType" v-model="passwd" id="passwd" name="passwd" class="form-control" @input="hideDiv()" required/>
                         <label for="passwd">Password:</label>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" :class="['bi','eye-icon',eye_class]" viewBox="0 0 16 16" @click="passwVisib()" v-html="eye_icon"></svg>
                     </div>
                 </div>
             </div>
@@ -87,6 +88,32 @@
     var showDismissibleAlert = ref(false);
     var showDismissibleAlertPassword = ref(false);
     var showDismissibleAlertUsername = ref(false);
+
+    var passwordType = ref('password');
+    var eye_class = ref('bi-eye-slash-fill');
+    var eye_icon = ref(`
+        <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
+        <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
+    `);
+
+    function passwVisib(){
+        if(passwordType.value == 'password'){
+            passwordType.value = 'text';
+            eye_icon.value = `
+                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+            `;
+            eye_class.value = 'bi-eye-fill';
+        }else{
+            passwordType.value = 'password';
+            eye_icon.value = `
+                <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
+                <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
+            `;
+            eye_class.value = 'bi-eye-slash-fill';
+        }
+        
+    }
 
     //ho utilizzato una srtinga invece di un indice perchè riutilizzerò il valore di formType per la richiesta al server 
     var formType = ref("Login");
@@ -141,3 +168,13 @@
         showDismissibleAlertUsername.value = false;
     }
 </script>
+
+<style scoped>
+    .eye-icon{
+        position: absolute;
+        right: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+        cursor: pointer;
+    }
+</style>
