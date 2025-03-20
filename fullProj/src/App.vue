1dddd2b77d2b437fb02a50817b37aa77373f9a53
zanny88 <117763771+zanny88@users.noreply.github.com>
2025-03-20 09:46:50 +0100

    toggle menu button fix

--- fullProj/src/App.vue
+++ fullProj/src/App.vue
@@ -1,74 +1,81 @@
 <template>
   <navbar></navbar>
-  <button class="menu-button" :style="buttonStyle" :class="{ open: isMenuOpen }" @click="toggleMenu">☰</button>
+  <button class="menu-button" :style="buttonStyle" :class="{ open: isMenuOpen, disabled: isMenuDisabled }" :disabled="isMenuDisabled" @click="toggleMenu">☰</button>
   <LateralMenu ref="lateralMenu" @menuToggled="handleMenuToggle" />
   <router-view></router-view>
   <TimeMachine ref="timeMachine" />
 </template>
 
 <script setup>
-import Navbar from './components/Navbar.vue';
-import TimeMachine from './components/TimeMachine.vue';
-import LateralMenu from './components/LateralMenu.vue';
-import axios from "axios";
-import { onMounted, inject, watch, provide, ref, nextTick, useTemplateRef, computed } from "vue";
-import { useRouter } from "vue-router";
+  import Navbar from './components/Navbar.vue';
+  import TimeMachine from './components/TimeMachine.vue';
+  import LateralMenu from './components/LateralMenu.vue';
+  import axios from "axios";
+  import { onMounted, inject, watch, provide, ref, nextTick, useTemplateRef, computed } from "vue";
+  import { useRouter } from "vue-router";
 
-const router = useRouter();
-const api_url = inject('api_url');
-var loggedIn = inject("loggedIn");
+  const router = useRouter();
+  const api_url = inject('api_url');
+  var loggedIn = inject("loggedIn");
 
-const timeMachineRef = useTemplateRef('timeMachine');
+  const timeMachineRef = useTemplateRef('timeMachine');
 
-const lateralMenuRef = useTemplateRef('lateralMenu');
-const isMenuOpen = ref(false);
-const menuWidth = 400; // Larghezza del menù in px
+  const lateralMenuRef = useTemplateRef('lateralMenu');
+  const isMenuOpen = ref(false);
+  const menuWidth = 400; // Larghezza del menù in px
+
+  var token = inject('IDtoken');
+  var isMenuDisabled = ref(token.value == null);
+
+  watch(token,(newValue) => {
+    isMenuDisabled.value = (newValue === null);
+  });
 
 
-// Funzione per aprire il menù dal Navbar
-const toggleMenu = async () => {
-  await nextTick(); // Assicura che Vue aggiorni il riferimento prima di accedere a toggleMenu()
-  if (lateralMenuRef.value) {
-    lateralMenuRef.value.toggleMenu();
-  } else {
-    console.error("Errore: lateralMenuRef è null");
-  }
-};
-
-const handleMenuToggle = (state) => {
-  isMenuOpen.value = state;
-};
-
-const buttonStyle = computed(() => ({
-  transform: isMenuOpen.value ? `translateX(-${menuWidth}px)` : "translateX(0)",
-  transition: "transform 0.3s ease-in-out"
-}));
-
-async function checkLogged() {
-  if (localStorage.getItem('token') === null) {
-    console.log("fai il login");
-    console.log(localStorage.getItem('token'));
-    router.push({ path: "/login" });
-  }
-}
-
-// Watch for route changes and set expanded to false
-watch(router.currentRoute, async() => {
-  await nextTick();
-  if (timeMachineRef.value) {
-    timeMachineRef.value.setExpanded(false);
-    if(router.currentRoute.value.path.includes("/pomodoro")) {
-      timeMachineRef.value.setDisabled(true);
+  // Funzione per aprire il menù dal Navbar
+  const toggleMenu = async () => {
+    await nextTick(); // Assicura che Vue aggiorni il riferimento prima di accedere a toggleMenu()
+    if (lateralMenuRef.value) {
+      lateralMenuRef.value.toggleMenu();
     } else {
-      timeMachineRef.value.setDisabled(false);
+      console.error("Errore: lateralMenuRef è null");
+    }
+  };
+
+  const handleMenuToggle = (state) => {
+    isMenuOpen.value = state;
+  };
+
+  const buttonStyle = computed(() => ({
+    transform: isMenuOpen.value ? `translateX(-${menuWidth}px)` : "translateX(0)",
+    transition: "transform 0.3s ease-in-out"
+  }));
+
+  async function checkLogged() {
+    if (localStorage.getItem('token') === null) {
+      console.log("fai il login");
+      console.log(localStorage.getItem('token'));
+      router.push({ path: "/login" });
     }
   }
-});
 
-// Check if the user is logged in when the component is mounted
-onMounted(() => {
-  checkLogged();
-});
+  // Watch for route changes and set expanded to false
+  watch(router.currentRoute, async() => {
+    await nextTick();
+    if (timeMachineRef.value) {
+      timeMachineRef.value.setExpanded(false);
+      if(router.currentRoute.value.path.includes("/pomodoro")) {
+        timeMachineRef.value.setDisabled(true);
+      } else {
+        timeMachineRef.value.setDisabled(false);
+      }
+    }
+  });
+
+  // Check if the user is logged in when the component is mounted
+  onMounted(() => {
+    checkLogged();
+  });
 </script>
 
 <style scoped>
@@ -84,6 +91,11 @@ onMounted(() => {
   padding: 10px 15px;
   border-radius: 5px;
   z-index: 1000;
+}
+
+.menu-button.disabled {
+  background: grey;
+  cursor: not-allowed;
 }
 
 @font-face {
