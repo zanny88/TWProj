18a959d755b6cbbd7d290b54c0ef19e4e77bd8af
zanny88 <117763771+zanny88@users.noreply.github.com>
2025-03-20 09:23:01 +0100

    added lateral menu for user2user msg

--- fullProj/src/App.vue
+++ fullProj/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <navbar></navbar>
+  <button class="menu-button" :style="buttonStyle" :class="{ open: isMenuOpen }" @click="toggleMenu">☰</button>
+  <LateralMenu ref="lateralMenu" @menuToggled="handleMenuToggle" />
   <router-view></router-view>
   <TimeMachine ref="timeMachine" />
 </template>
@@ -7,8 +9,9 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
 import TimeMachine from './components/TimeMachine.vue';
+import LateralMenu from './components/LateralMenu.vue';
 import axios from "axios";
-import { onMounted, inject, watch, provide, ref, nextTick, useTemplateRef } from "vue";
+import { onMounted, inject, watch, provide, ref, nextTick, useTemplateRef, computed } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
@@ -16,6 +19,30 @@ const api_url = inject('api_url');
 var loggedIn = inject("loggedIn");
 
 const timeMachineRef = useTemplateRef('timeMachine');
+
+const lateralMenuRef = useTemplateRef('lateralMenu');
+const isMenuOpen = ref(false);
+const menuWidth = 400; // Larghezza del menù in px
+
+
+// Funzione per aprire il menù dal Navbar
+const toggleMenu = async () => {
+  await nextTick(); // Assicura che Vue aggiorni il riferimento prima di accedere a toggleMenu()
+  if (lateralMenuRef.value) {
+    lateralMenuRef.value.toggleMenu();
+  } else {
+    console.error("Errore: lateralMenuRef è null");
+  }
+};
+
+const handleMenuToggle = (state) => {
+  isMenuOpen.value = state;
+};
+
+const buttonStyle = computed(() => ({
+  transform: isMenuOpen.value ? `translateX(-${menuWidth}px)` : "translateX(0)",
+  transition: "transform 0.3s ease-in-out"
+}));
 
 async function checkLogged() {
   if (localStorage.getItem('token') === null) {
@@ -45,6 +72,20 @@ onMounted(() => {
 </script>
 
 <style scoped>
+.menu-button {
+  position: fixed;
+  top: 58px;
+  right: 0px; /* Posizionato al margine destro */
+  background: black;
+  border: none;
+  color: white;
+  font-size: 24px;
+  cursor: pointer;
+  padding: 10px 15px;
+  border-radius: 5px;
+  z-index: 1000;
+}
+
 @font-face {
   font-family: Ginto;
   src: url('/public/fonts/ABCGintoNormal-Regular-Trial-BF651b7b7846685.otf');
