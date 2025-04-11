42f93c4d6bf7f9bdd16b8bc6fb20133e167ba48e
Lorenzo Giarrusso <lorenzo.giarrusso@studio.unibo.it>
2025-04-11 19:04:13 +0200

    login/register form

--- fullProj/src/components/registerLogin.vue
+++ fullProj/src/components/registerLogin.vue
@@ -1,75 +1,79 @@
 <template>
-    <div class="container" style="text-align: center; margin-top: 10px;">
-        <h5 id="title"> {{ formType }} form</h5>
-        <form id="mainForm">
-            <div class="row" style="position: relative; justify-content: center; align-items: center;" v-if="formType == 'Register'">
-                <div class="col-lg-2 col-md-2 col-sm-4">
-                    <div class="form-floating">
-                        <input type="text" v-model="firstName" id="firstName" name="firstName" class="form-control" required/>
-                        <label for="firstName">First name: </label>
+    <div class="background"></div>
+    <div class="form-container">
+        <div class="form-card">
+            <h5 id="title" class="form-title">{{ formType }} Form</h5>
+            <form id="mainForm">
+                <div class="row" v-if="formType == 'Register'">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="form-floating">
+                            <input type="text" v-model="firstName" id="firstName" name="firstName" class="form-control" required />
+                            <label for="firstName">First Name</label>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="form-floating">
+                            <input type="text" v-model="lastName" id="lastName" name="lastName" class="form-control" required />
+                            <label for="lastName">Last Name</label>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-4">
-                    <div class="form-floating">
-                        <input type="text" v-model="lastName" id="lastName" name="lastName" class="form-control" required/>
-                        <label for="lastName">Last name: </label>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-floating">
+                            <input type="text" v-model="name" id="username" name="username" class="form-control" @input="hideDiv()" required />
+                            <label for="username">Username</label>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="row" style="position: relative; justify-content: center; align-items: center;">
-                <div class="col-lg-4 col-md-4 col-sm-12">
-                    <div class="form-floating">
-                        <input type="text" v-model="name" id="username" name="username" class="form-control" @input="hideDiv()" required/>
-                        <label for="username">Username: </label>
+                <div v-show="showDismissibleAlert" class="alert alert-danger">
+                    Username already taken!
+                </div>
+                <div v-show="showDismissibleAlertUsername" class="alert alert-danger">
+                    Username not found!
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-floating password-container">
+                            <input :type="passwordType" v-model="passwd" id="passwd" name="passwd" class="form-control" @input="hideDiv()" required />
+                            <label for="passwd">Password</label>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" :class="['bi', 'eye-icon', eye_class]" viewBox="0 0 16 16" @click="passwVisib()" v-html="eye_icon"></svg>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div v-show="showDismissibleAlert" style="background: red; color: black;">
-                Username already taken!
-            </div>
-            <div v-show="showDismissibleAlertUsername" style="background: red; color: black;">
-                Username not found!
-            </div>
-            <div class="row" style="position: relative; justify-content: center; align-items: center;">
-                <div class="col-lg-4 col-md-4 col-sm-12">
-                    <div class="form-floating" style="position: relative;">
-                        <input :type="passwordType" v-model="passwd" id="passwd" name="passwd" class="form-control" @input="hideDiv()" required/>
-                        <label for="passwd">Password:</label>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" :class="['bi','eye-icon',eye_class]" viewBox="0 0 16 16" @click="passwVisib()" v-html="eye_icon"></svg>
+                <div v-show="showDismissibleAlertPassword" class="alert alert-danger">
+                    Wrong password!
+                </div>
+                <div class="row" v-if="formType == 'Register'">
+                    <div class="col-12">
+                        <div class="form-floating">
+                            <input type="email" v-model="mail" id="mail" name="mail" class="form-control" required />
+                            <label for="mail">Email</label>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div v-show="showDismissibleAlertPassword" style="background: red; color: black;">
-                Wrong password!
-            </div>
-            <div class="row" style="position: relative; justify-content: center; align-items: center;" v-if="formType == 'Register'">
-                <div class="col-lg-4 col-md-4 col-sm-12" >
-                    <div class="form-floating">
-                        <input type="email" v-model="mail" id="mail" name="mail" class="form-control" required/>
-                        <label for="mail">Mail: </label>
+                <div class="row mt-4">
+                    <div class="col-12">
+                        <button type="button" class="btn btn-primary w-100" id="mainButton" @click="submit()">{{ formType }}</button>
                     </div>
                 </div>
-            </div>
-            <div class="row" style="position: relative; justify-content: center; align-items: center; margin-top: 10px;">
-                <div class="col-lg-2 col-md-2 col-sm-4">
-                    <button type="button" class="btn btn-outline-info" id="mainButton" @click="submit()">{{ formType }}</button>
+            </form>
+            <div class="row mt-3">
+                <div class="col-12 text-center">
+                    <p id="bottomText">
+                        <b>
+                            {{ outlines[formType][0] }}
+                            <a href="#" @click.prevent="changeForm()" class="form-link">
+                                {{ outlines[formType][1] }}
+                            </a>
+                        </b>
+                    </p>
                 </div>
-            </div>
-        </form>
-        <div class="row" style="position: relative; justify-content: center; align-items: center;">
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <p id='bottomText'>
-                    <b>
-                        {{ outlines[formType][0] }}
-                        <a href='#' @click.prevent='changeForm()'>
-                            {{ outlines[formType][1] }}
-                        </a>
-                    </b>
-                </p>
             </div>
         </div>
     </div>
 </template>
+
 <script setup>
     import {ref,inject} from "vue";
     import axios from "axios";
@@ -172,11 +176,94 @@
 </script>
 
 <style scoped>
-    .eye-icon{
-        position: absolute;
-        right: 10px;
-        top: 50%;
-        transform: translateY(-50%);
-        cursor: pointer;
-    }
+.background {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    z-index: -1;
+    background: linear-gradient(to right, #f8f9fa, #e9ecef);
+}
+
+/* General container styling */
+.form-container {
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+}
+
+/* Card styling */
+.form-card {
+    background: white;
+    padding: 30px;
+    border-radius: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    width: 100%;
+    max-width: 500px;
+}
+
+/* Title styling */
+.form-title {
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-bottom: 20px;
+    text-align: center;
+}
+
+/* Input fields */
+.form-floating {
+    margin-bottom: 15px;
+}
+
+.form-control {
+    border-radius: 5px;
+}
+
+/* Password visibility icon */
+.password-container {
+    position: relative;
+}
+
+.eye-icon {
+    position: absolute;
+    right: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+    cursor: pointer;
+}
+
+/* Button styling */
+.btn-primary {
+    background-color: #007bff;
+    border: none;
+    transition: background-color 0.3s ease;
+}
+
+.btn-primary:hover {
+    background-color: #0056b3;
+}
+
+/* Alert styling */
+.alert {
+    margin-top: 10px;
+    padding: 10px;
+    border-radius: 5px;
+    font-size: 0.9rem;
+}
+
+/* Link styling */
+.form-link {
+    color: #007bff;
+    text-decoration: none;
+    transition: color 0.3s ease;
+}
+
+.form-link:hover {
+    color: #0056b3;
+}
 </style>
